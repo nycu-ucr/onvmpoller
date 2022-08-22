81381a2db276ccfb8807514266abd13d4e5d6855
Hanson-Tsai <jerry10127@gmail.com>
2022-08-22 13:59:29 +0000

    ONVM Listen fatal bug fixed

---
+++ listen.go
@@ -0,0 +1,42 @@
+package onvmpoller
+
+import (
+	"fmt"
+	"net"
+)
+
+func listen(ip_addr string, port uint16) (net.Listener, error) {
+	var l net.Listener
+	var err error
+	l, err = listenONVM(ip_addr, port)
+	if err != nil {
+		return nil, err
+	}
+
+	return l, nil
+}
+
+func listenONVM(ip_addr string, port uint16) (*OnvmListener, error) {
+	c := &Connection{
+		conn_id: LISTENER_CONN_ID,
+		rxchan:  make(chan RxChannelData, 1),
+		txchan:  make(chan TxChannelData, 1),
+	}
+	c.four_tuple[SRC_IP_ADDR_IDX] = ip_addr
+	c.four_tuple[SRC_PORT_IDX] = fmt.Sprint(port)
+	onvmpoll.Add(c)
+
+	id, err := IpToID(ip_addr)
+	laddr := OnvmAddr{
+		service_id: uint8(id),
+		network:    "onvm",
+		ipv4_addr:  ip_addr,
+		port:       port,
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &OnvmListener{laddr: laddr, conn: c}, nil
+}
--- poller.go
+++ poller.go
@@ -1,12 +1,12 @@
 package onvmpoller
 
 // #cgo CFLAGS: -m64 -pthread -O3 -march=native
-// #cgo CFLAGS: -I/home/johnson/l25gc/onvm-upf/onvm/onvm_nflib
-// #cgo CFLAGS: -I/home/johnson/l25gc/onvm-upf/onvm/lib
-// #cgo CFLAGS: -I/home/johnson/l25gc/onvm-upf/dpdk/x86_64-native-linuxapp-gcc/include
-// #cgo LDFLAGS: /home/johnson/l25gc/onvm-upf/onvm/onvm_nflib/x86_64-native-linuxapp-gcc/libonvm.a
-// #cgo LDFLAGS: /home/johnson/l25gc/onvm-upf/onvm/lib/x86_64-native-linuxapp-gcc/lib/libonvmhelper.a -lm
-// #cgo LDFLAGS: -L/home/johnson/l25gc/onvm-upf/dpdk/x86_64-native-linuxapp-gcc/lib
+// #cgo CFLAGS: -I<replace path>/onvm-upf/onvm/onvm_nflib
+// #cgo CFLAGS: -I<replace path>/onvm-upf/onvm/lib
+// #cgo CFLAGS: -I<replace path>/onvm-upf/dpdk/x86_64-native-linuxapp-gcc/include
+// #cgo LDFLAGS: <replace path>/onvm-upf/onvm/onvm_nflib/x86_64-native-linuxapp-gcc/libonvm.a
+// #cgo LDFLAGS: <replace path>/onvm-upf/onvm/lib/x86_64-native-linuxapp-gcc/lib/libonvmhelper.a -lm
+// #cgo LDFLAGS: -L<replace path>/onvm-upf/dpdk/x86_64-native-linuxapp-gcc/lib
 // #cgo LDFLAGS: -lrte_flow_classify -Wl,--whole-archive -lrte_pipeline -Wl,--no-whole-archive -Wl,--whole-archive -lrte_table -Wl,--no-whole-archive -Wl,--whole-archive -lrte_port -Wl,--no-whole-archive -lrte_pdump -lrte_distributor -lrte_ip_frag -lrte_meter -lrte_fib -lrte_rib -lrte_lpm -lrte_acl -lrte_jobstats -Wl,--whole-archive -lrte_metrics -Wl,--no-whole-archive -lrte_bitratestats -lrte_latencystats -lrte_power -lrte_efd -lrte_bpf -lrte_ipsec -Wl,--whole-archive -lrte_cfgfile -lrte_gro -lrte_gso -lrte_hash -lrte_member -lrte_vhost -lrte_kvargs -lrte_telemetry -lrte_mbuf -lrte_net -lrte_ethdev -lrte_bbdev -lrte_cryptodev -lrte_security -lrte_compressdev -lrte_eventdev -lrte_rawdev -lrte_timer -lrte_mempool -lrte_stack -lrte_mempool_ring -lrte_mempool_octeontx2 -lrte_ring -lrte_pci -lrte_eal -lrte_cmdline -lrte_reorder -lrte_sched -lrte_rcu -lrte_graph -lrte_node -lrte_kni -lrte_common_cpt -lrte_common_octeontx -lrte_common_octeontx2 -lrte_common_dpaax -lrte_bus_pci -lrte_bus_vdev -lrte_bus_dpaa -lrte_bus_fslmc -lrte_mempool_bucket -lrte_mempool_stack -lrte_mempool_dpaa -lrte_mempool_dpaa2 -lrte_pmd_af_packet -lrte_pmd_ark -lrte_pmd_atlantic -lrte_pmd_avp -lrte_pmd_axgbe -lrte_pmd_bnxt -lrte_pmd_bond -lrte_pmd_cxgbe -lrte_pmd_dpaa -lrte_pmd_dpaa2 -lrte_pmd_e1000 -lrte_pmd_ena -lrte_pmd_enetc -lrte_pmd_enic -lrte_pmd_fm10k -lrte_pmd_failsafe -lrte_pmd_hinic -lrte_pmd_hns3 -lrte_pmd_i40e -lrte_pmd_iavf -lrte_pmd_ice -lrte_common_iavf -lrte_pmd_igc -lrte_pmd_ionic -lrte_pmd_ixgbe -lrte_pmd_kni -lrte_pmd_lio -lrte_pmd_memif -lrte_pmd_nfp -lrte_pmd_null -lrte_pmd_octeontx2 -lrte_pmd_qede -lrte_pmd_ring -lrte_pmd_softnic -lrte_pmd_sfc_efx -lrte_pmd_tap -lrte_pmd_thunderx_nicvf -lrte_pmd_vdev_netvsc -lrte_pmd_virtio -lrte_pmd_vhost -lrte_pmd_ifc -lrte_pmd_vmxnet3_uio -lrte_bus_vmbus -lrte_pmd_netvsc -lrte_pmd_bbdev_null -lrte_pmd_bbdev_fpga_lte_fec -lrte_pmd_bbdev_fpga_5gnr_fec -lrte_pmd_bbdev_turbo_sw -lrte_pmd_null_crypto -lrte_pmd_nitrox -lrte_pmd_octeontx_crypto -lrte_pmd_octeontx2_crypto -lrte_pmd_crypto_scheduler -lrte_pmd_dpaa2_sec -lrte_pmd_dpaa_sec -lrte_pmd_caam_jr -lrte_pmd_virtio_crypto -lrte_pmd_octeontx_zip -lrte_pmd_qat -lrte_pmd_skeleton_event -lrte_pmd_sw_event -lrte_pmd_dsw_event -lrte_pmd_octeontx_ssovf -lrte_pmd_dpaa_event -lrte_pmd_dpaa2_event -lrte_mempool_octeontx -lrte_pmd_octeontx -lrte_pmd_octeontx2_event -lrte_pmd_opdl_event -lrte_rawdev_skeleton -lrte_rawdev_dpaa2_cmdif -lrte_rawdev_dpaa2_qdma -lrte_bus_ifpga -lrte_rawdev_ioat -lrte_rawdev_ntb -lrte_rawdev_octeontx2_dma -lrte_rawdev_octeontx2_ep -Wl,--no-whole-archive -lrt -lm -lnuma -ldl
 /*
 #include <onvm_nflib.h>
@@ -78,12 +78,12 @@ type Connection struct {
 }
 
 type OnvmListener struct {
-	laddr OnvmAddr   // Local Address
-	conn  Connection // This need to handle the incoming connection
+	laddr OnvmAddr    // Local Address
+	conn  *Connection // This need to handle the incoming connection
 }
 
 type OnvmAddr struct {
-	srevice_id uint8 // Service ID of NF
+	service_id uint8 // Service ID of NF
 	network    string
 	ipv4_addr  string
 	port       uint16
@@ -114,6 +114,7 @@ func init() {
 	/* Initialize Global Variable */
 	InitConfig()
 	conn_id = 0
+	local_address = "127.0.0.1"
 	port_pool = make(map[uint16]bool)
 	onvmpoll.tables = sync.Map{}
 	nf_pkt_handler_chan = make(chan RxChannelData, 5)
@@ -459,7 +460,8 @@ func (oa OnvmAddr) Network() string {
 }
 
 func (oa OnvmAddr) String() string {
-	s := fmt.Sprintf("Network: %s, Service ID: %2d, IP Address: %s, Port: %5d.", oa.network, oa.srevice_id, oa.ipv4_addr, oa.port)
+	s := fmt.Sprintf("Network: %s, Service ID: %2d, IP Address: %s, Port: %5d.",
+		oa.network, oa.service_id, oa.ipv4_addr, oa.port)
 	return s
 }
 
@@ -518,7 +520,7 @@ func (connection Connection) LocalAddr() net.Addr {
 	oa.port = uint16(v)
 	oa.network = "onvm"
 	id, _ := IpToID(oa.ipv4_addr)
-	oa.srevice_id = uint8(id)
+	oa.service_id = uint8(id)
 
 	return oa
 }
@@ -531,7 +533,7 @@ func (connection Connection) RemoteAddr() net.Addr {
 	oa.port = uint16(v)
 	oa.network = "onvm"
 	id, _ := IpToID(oa.ipv4_addr)
-	oa.srevice_id = uint8(id)
+	oa.service_id = uint8(id)
 
 	return oa
 }
@@ -607,33 +609,15 @@ func CreateConnection() net.Conn {
 }
 
 func ListenONVM(network, address string) (net.Listener, error) {
-	var ol *OnvmListener
 	if network != "onvm" {
 		msg := fmt.Sprintf("Unsppourt network type: %v", network)
 		err := errors.New(msg)
-		return ol, err
+		return nil, err
 	}
 	ip_addr, port := ParseAddress(address)
 	local_address = ip_addr
 
-	/* Initialize OnvmListener */
-	id, _ := IpToID(ip_addr)
-	var conn Connection
-	conn.rxchan = make(chan RxChannelData, 1) // Listener's rxchan must be block, wait for incoming connection
-	conn.txchan = make(chan TxChannelData, 1) // This won't be used
-	conn.conn_id = LISTENER_CONN_ID
-
-	ol.conn = conn
-	ol.conn.four_tuple[SRC_IP_ADDR_IDX] = ip_addr
-	ol.conn.four_tuple[SRC_PORT_IDX] = fmt.Sprint(port)
-	ol.laddr.port = port
-	ol.laddr.network = network
-	ol.laddr.ipv4_addr = ip_addr
-	ol.laddr.srevice_id = uint8(id)
-
-	onvmpoll.Add(&ol.conn)
-
-	return ol, nil
+	return listen(ip_addr, port)
 }
 
 func DialONVM(network, address string) (net.Conn, error) {
